3e411a3063b94f9802611b42fd6f9ddbd4ab4f1d
Alpacorp <alejandro.palacios88@gmail.com>
2022-11-09 13:52:52 -0500

    fix new styles to width to fields forms

--- src/components/DataTypeForm2.tsx
+++ src/components/DataTypeForm2.tsx
@@ -37,62 +37,71 @@ const DataTypeForm2 = ({ title }: PropsDataForm2) => {
   };
 
   return (
-    <div style={{ width: "100%" }}>
+    <div>
       <h3>Registro de {title}</h3>
-      <form onSubmit={handleNewData}>
-        <TextField
-          id="addName"
-          name="addName"
-          label="Nombre"
-          value={addName}
-          onChange={onInputChange}
-          variant="outlined"
-          size="small"
-          error={false}
-          required
-          type="text"
-          style={{ width: "100%", marginBottom: "10px" }}
-          helperText="Digital el nombre"
-        />
-        <TextField
-          id="addShortName"
-          name="addShortName"
-          label="Nombre corto"
-          value={addShortName}
-          onChange={onInputChange}
-          variant="outlined"
-          size="small"
-          error={false}
-          required
-          type="text"
-          style={{ width: "100%", marginBottom: "10px" }}
-          helperText="Digital el nombre corto el cual debe ser único"
-        />
-        <TextField
-          id="addIdChannelType"
-          name="addIdChannelType"
-          label="Tipo de Canal"
-          value={addIdChannelType}
-          onChange={onInputChange}
-          variant="outlined"
-          size="small"
-          error={false}
-          required
-          type="number"
-          style={{ width: "100%", marginBottom: "10px" }}
-          helperText="Digital el número de tipo de canal"
-        />
-        <Grid style={{ margin: "10px 0 30px 0" }}>
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            disabled={addName.length === 0 || addShortName.length === 0}
-          >
-            <Typography variant="button">Registrar {title}</Typography>
-          </Button>
+      <Grid
+        container
+        style={{ margin: "20px 0", justifyContent: "center" }}
+        itemScope
+        itemType="https://schema.org/Thing"
+      >
+        <Grid item lg={6} md={12} xs={12}>
+          <form onSubmit={handleNewData}>
+            <TextField
+              id="addName"
+              name="addName"
+              label="Nombre"
+              value={addName}
+              onChange={onInputChange}
+              variant="outlined"
+              size="small"
+              error={false}
+              required
+              type="text"
+              style={{ width: "100%", marginBottom: "10px" }}
+              helperText="Digital el nombre"
+            />
+            <TextField
+              id="addShortName"
+              name="addShortName"
+              label="Nombre corto"
+              value={addShortName}
+              onChange={onInputChange}
+              variant="outlined"
+              size="small"
+              error={false}
+              required
+              type="text"
+              style={{ width: "100%", marginBottom: "10px" }}
+              helperText="Digital el nombre corto el cual debe ser único"
+            />
+            <TextField
+              id="addIdChannelType"
+              name="addIdChannelType"
+              label="Tipo de Canal"
+              value={addIdChannelType}
+              onChange={onInputChange}
+              variant="outlined"
+              size="small"
+              error={false}
+              required
+              type="number"
+              style={{ width: "100%", marginBottom: "10px" }}
+              helperText="Digital el número de tipo de canal"
+            />
+            <Grid style={{ margin: "10px 0 30px 0" }}>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                disabled={addName.length === 0 || addShortName.length === 0}
+              >
+                <Typography variant="button">Registrar {title}</Typography>
+              </Button>
+            </Grid>
+          </form>
         </Grid>
-      </form>
+      </Grid>
     </div>
   );
 };
--- src/containers/AppRouter.tsx
+++ src/containers/AppRouter.tsx
@@ -32,38 +32,54 @@ const AppRouter = () => {
         <Layout>
           <div
             className="mainLogo"
-            style={{ maxWidth: "300px", width: "100%", margin: "0 auto" }}
+            style={{
+              // maxWidth: "300px",
+              width: "100%",
+              // margin: "0 auto",
+              display: "flex",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+            }}
             itemScope
             itemType="http://schema.org/Brand"
           >
-            <img
-              alt="logo del banco caja social"
-              itemProp="logo"
-              src="https://www.bancocajasocial.com/portalserver/content/atom/ed3567c4-64a3-462a-93a4-7c6466ef50e8/content/General/Logo%20Banco%20Caja%20Social?id=ff6b0aed-6fb8-4be2-9326-cb5847feac22"
-            />
+            <div
+              style={{
+                width: "100%",
+                maxWidth: "300px",
+              }}
+            >
+              <img
+                alt="logo del banco caja social"
+                itemProp="logo"
+                src="https://www.bancocajasocial.com/portalserver/content/atom/ed3567c4-64a3-462a-93a4-7c6466ef50e8/content/General/Logo%20Banco%20Caja%20Social?id=ff6b0aed-6fb8-4be2-9326-cb5847feac22"
+              />
+            </div>
 
             {status === "authenticated" ? (
-              <Grid style={{ margin: "20px 0" }}>
-                <Typography variant="h6" color="primary" fontSize={18}>
-                  Hola <strong>{user?.name || "Usuario"}</strong>
-                </Typography>
-                <Typography
-                  variant="inherit"
-                  color="primary"
-                  fontSize={13}
-                  style={{ margin: "5px 0" }}
-                >
-                  Perfil <strong>{user.role || "Rol"}</strong>
-                </Typography>
-                <Button
-                  startIcon={<ExitToAppIcon />}
-                  size="small"
-                  onClick={startLogout}
-                  variant="outlined"
-                >
-                  Cerrar Sesión
-                </Button>
-              </Grid>
+              <div>
+                <Grid style={{ margin: "20px 0" }}>
+                  <Typography variant="h6" color="primary" fontSize={18}>
+                    Hola <strong>{user?.name || "Usuario"}</strong>
+                  </Typography>
+                  <Typography
+                    variant="inherit"
+                    color="primary"
+                    fontSize={13}
+                    style={{ margin: "5px 0" }}
+                  >
+                    Perfil <strong>{user.role || "Rol"}</strong>
+                  </Typography>
+                  <Button
+                    startIcon={<ExitToAppIcon />}
+                    size="small"
+                    onClick={startLogout}
+                    variant="outlined"
+                  >
+                    Cerrar Sesión
+                  </Button>
+                </Grid>
+              </div>
             ) : (
               ""
             )}
