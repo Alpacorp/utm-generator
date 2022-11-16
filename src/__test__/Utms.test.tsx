cc262eed1c8c9c09414b7aac1c435ad0cec2f93f
Alpacorp <alejandro.palacios88@gmail.com>
2022-11-15 23:23:29 -0500

    add two new test to render components

---
+++ src/__test__/Utms.test.tsx
@@ -0,0 +1,31 @@
+// create a test suite for the utms page
+
+import { render, screen } from "@testing-library/react";
+import Utms from "../components/Utms";
+import { BrowserRouter } from "react-router-dom";
+
+describe("Utms Page", () => {
+  // create a test case for the utms page
+  it("should render the utms page", () => {
+    // render the utms page
+    render(
+      <BrowserRouter>
+        <Utms />
+      </BrowserRouter>
+    );
+    // check if the utms page is rendered
+    expect(screen.getByText("UTMs")).toBeInTheDocument();
+  });
+
+  // create a test case for the utms form
+  it("should render the utms form", () => {
+    // render the utms page
+    render(
+      <BrowserRouter>
+        <Utms />
+      </BrowserRouter>
+    );
+    // check if the utms form is rendered
+    expect(screen.getByRole("form")).toBeInTheDocument();
+  });
+});
